a1bf6e6aeb93627b4302b150a5351afd38c81a70
TOUZE Alexandre <alexandre.touze@orange.com>
2017-03-27 18:05:24 +0200

    News Carrousel SPFx

---
+++ src/webparts/newsCarrousel/INewsCarrouselWebPartProps.ts
@@ -0,0 +1,3 @@
+export interface INewsCarrouselWebPartProps {
+  description: string;
+}
---
+++ src/webparts/newsCarrousel/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface INewsCarrouselStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'newsCarrouselStrings' {
+  const strings: INewsCarrouselStrings;
+  export = strings;
+}
